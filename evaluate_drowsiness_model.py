79c5db9e4f279135b03d4655aff7ee4dd1a58979
APAC\nel8kor <NagendraBabu.Ellapu@in.bosch.com>
2025-05-21 14:51:02 +0530

    Fix label format by converting y_seq to categorical for model evaluation

--- evaluate_drowsiness_model.py
+++ evaluate_drowsiness_model.py
@@ -29,7 +29,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 # Load test data
 X_seq = np.load('saved_drowsy_npy\X_seq.npy')
 y_seq = np.load('saved_drowsy_npy\y_seq.npy')
-y_seq = to_categorical(y_seq, num_classes=2)
+
 print(f"X_Seq shape: {X_seq.shape}")
 print(f"Y_seq shape: {y_seq.shape}")
 
